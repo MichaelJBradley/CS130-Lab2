457dcfb264b4ebcf5259ee6cb960fa5b5c309cdf
Michael Bradley <mbrad94@gmail.com>
2019-01-15 19:07:34 -0800

    Normalize direction vector in Render_Pixel function

--- render_world.cpp
+++ render_world.cpp
@@ -42,8 +42,10 @@ Hit Render_World::Closest_Intersection(const Ray& ray)
 // set up the initial view ray and call
 void Render_World::Render_Pixel(const ivec2& pixel_index)
 {
+    // Calculate the direction vector from camera to the pixel
     vec3 dir = camera.World_Position(pixel_index) - camera.position;
-    Ray ray(camera.position, dir);
+    // Create the ray from camera with normalized direction vector
+    Ray ray(camera.position, dir.normalized());
     vec3 color=Cast_Ray(ray,1);
     camera.Set_Pixel(pixel_index,Pixel_Color(color));
 }
