f88a82c520e01bf523354bbd81a8fc881577edd9
Michael Bradley <mbrad94@gmail.com>
2019-01-16 21:57:01 -0800

    Implement Sphere::Intersection function

--- sphere.cpp
+++ sphere.cpp
@@ -1,11 +1,44 @@
 #include "sphere.h"
 #include "ray.h"
 
+#include <cmath>
+
 // Determine if the ray intersects with the sphere
 Hit Sphere::Intersection(const Ray& ray, int part) const
 {
-    TODO;
-    return {0,0,0};
+    double t1;
+    double t2;
+    Hit ret;
+    ret.object = NULL;
+    ret.part = -1;
+
+    vec3 u = ray.direction;
+    vec3 w = ray.end_point - center;
+    double a = u.magnitude_squared();
+    double b = 2 * (u * w);
+    double c = w.magnitude_squared() - radius;
+    double descrim = (b * b) - (4 * a * c);
+
+    // The ray does not intersect
+    if (descrim < 0) {
+        return ret;
+    }
+
+    // Calculated an intersection
+    t1 = (-b + sqrt(descrim)) / 2 * a * c;
+    t2 = (-b - sqrt(descrim)) / 2 * a * c;
+        
+    // Determine which is the closer intersection
+    // And whether it beats the threshold
+    if (t1 < t2 && t1 >= small_t) {
+        ret.dist = t1;
+        ret.object = this;
+    } else if (t2 <= t1 && t2 >= small_t) {
+        ret.dist = t2;
+        ret.object = this;
+    }
+
+    return ret;
 }
 
 vec3 Sphere::Normal(const vec3& point, int part) const
