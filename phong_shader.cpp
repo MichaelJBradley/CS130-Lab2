68f1f3e112066ef7cb8dbcb6b4ab942a7a4e32bd
Michael Bradley <mbrad94@gmail.com>
2019-02-03 00:17:39 -0800

    Implement shadows

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -24,6 +24,14 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     // iterate through world lights
     for (unsigned i = 0; i < world.lights.size(); i++) { 
         vec3 l = world.lights[i]->position - intersection_point;
+
+        // check whether object is in shadow
+        Ray to_light(intersection_point, l);
+        Hit hit = world.Closest_Intersection(to_light);
+        vec3 obj_to_hit = to_light.Point(hit.dist) - intersection_point;
+        if (!world.enable_shadows || (!hit.object || 
+            obj_to_hit.magnitude() > l.magnitude())) {
+
         vec3 r = (-l + 2 * dot(l, normal) * normal).normalized();
 
         intense_dif += color_diffuse 
@@ -34,7 +42,10 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
                      * world.lights[i]->Emitted_Light(l)
                      * std::pow(std::max(dot(r, -(ray.direction)), 0.0),
                                 specular_power);
+        }
+        
     }
+
 
     return intense_amb + intense_dif + intense_spec;
 }
