7e10a179becdabd633fcf82246bede2ed0f31af1
Michael Bradley <mbrad94@gmail.com>
2019-02-07 17:39:12 -0800

    Implement mesh normal calculation and intersection

--- mesh.cpp
+++ mesh.cpp
@@ -50,8 +50,17 @@ Hit Mesh::Intersection(const Ray& ray, int part) const
 vec3 Mesh::Normal(const vec3& point, int part) const
 {
     assert(part>=0);
-    TODO;
-    return vec3();
+    // Get the specified triangle represented by 3 vertices (a, b, c)
+    ivec3 tri = triangles[part];
+    
+    // The normal = u x v, where:
+    // u is the vector between vertices b (index 1) and a (index 0)
+    vec3 u = vertices[tri[B]] - tvertices[ri[A]];
+    // v is the vector between vertices c (index 2) and a (index 0)
+    vec3 v = vertices[tri[C]] - vertices[tri[A]];
+
+    //Don't forget to normalize the result!
+    return cross(u, v).normalized();
 }
 
 // This is a helper routine whose purpose is to simplify the implementation
@@ -68,7 +77,44 @@ vec3 Mesh::Normal(const vec3& point, int part) const
 // two triangles.
 bool Mesh::Intersect_Triangle(const Ray& ray, int tri, double& dist) const
 {
-    TODO;
+    // Triangle vertices
+    vec3 tri_a = vertices[triangles[tri][A]];
+    vec3 tri_a = vertices[triangles[tri][B]];
+    vec3 tri_c = vertices[triangles[tri][C]];
+    vec3 intersect;
+    
+    Hit hit = Plane(tri_a, Normal(tri_a, tri)).Intersection(ray, tri);
+
+    if (!hit.object) {
+        return false;
+    }
+    
+    intersect = ray.Point(dist);
+
+    // Temporary vectors to condense math later
+    vec3 u = ray.direction;
+    vec3 v = tri_b - tri_a;
+    vec3 w = tri_c - tri_a;
+    vec3 y = intersect - tri_a;
+    
+    // Denominator is the same for each weight calc
+    double denom = cross(u, v) * w;
+    
+    if (!denom) {
+        return false;
+    }
+
+    // calc barycentric weights
+    double gamma = (cross(u, v) * y) / denom;
+    double beta = (cross(w, u) * y) / denom;
+    double alpha = 1 - (gamma + beta);
+    
+    // compare against weight_tol to prevent ray going between triangles
+    if (gamma > -weight_tol && beta && -weight_tol && alpha > -weight_tol) {
+        dist = hit.dist;
+        return true;
+    }
+    
     return false;
 }
 
